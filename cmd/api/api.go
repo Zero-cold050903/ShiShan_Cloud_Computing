25167a5de1bfe327ce565d3bd2f3a4987d2a0cf7
dylanliu <pxyforeverlove1314@gmail.com>
2025-05-28 16:11:44 +0800

    remove the routes pack and implement it directly in the api pack

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,26 +1,213 @@
 package api
 
 import (
+	"os"
 	"github.com/gin-gonic/gin"
 )
-
-func uploadDocument(c *gin.Context) {
-
+var router = gin.Default()
+var upload = router.Group("/upload")
+var download = router.Group("/download")
+var file *gin.FileHeader
+var err error
+func OpenULapi() {
+	// 上传文件
+	upload.POST("/document", uploadDocument)
+	upload.POST("/music", uploadMusic)
+	upload.POST("/file", uploadFile)
+	upload.POST("/image", uploadImage)
+	router.Run(":8080")
 }
-func uploadMusic(c *gin.Context) {
-
+func CloseULapi() {
+	router = nil
+	upload = nil
+	file = nil
+	err = nil
 }
-func uploadFile(c *gin.Context) {
-
+func OpenDLapi() {
+	//
+	download.GET("/document",downloadDocument)
+	download.GET("/music",downloadMusic)
+	download.GET("/file",downloadFile)
+	download.GET("/image",downloadImage)
+	router.Run(":8080")
 }
-func uploadVideo(c *gin.Context) {
-
+func CloseDLapi() {
+	router = nil
+	download = nil
+	file = nil
+	err = nil
 }
-func uploadPicture(c *gin.Context) {
-
+func uploadDocument(c *gin.Context) error {
+	// 上传文件
+	file, err := c.FormFile("file")
+	if err != nil {
+		routes.ErrorResponse(c, 400, "上传文件失败")
+		return gin.Error{
+			Err:  err,
+			Type: 0,
+			Meta: nil,
+		}
+	}
+	err = os.Mkdir("./USERFILE", 0777)
+	if err != nil {
+		routes.ErrorResponse(c, 500, "创建文件夹失败")
+		return gin.Error{
+			Err:  err,
+			Type: 0,
+			Meta: nil,
+		}
+	}
+	err = c.SaveUploadedFile(file, "./USERFILE/"+file.Filename)
+	if err != nil {
+		routes.ErrorResponse(c, 500, "保存文件失败")
+		return gin.Error{
+			Err:  err,
+			Type: 0,
+			Meta: nil,
+		}
+	}
+	routes.SuccessResponse(c, 200, "上传文件成功", gin.H{
+		"filename": file.Filename,
+	})
+	return nil
 }
-func downloadDocument(c *gin.Context) {
 
+func uploadMusic(c *gin.Context) error {
+	// 上传文件
+	file, err := c.FormFile("file")
+	if err!= nil {
+		routes.ErrorResponse(c, 400, "上传文件失败")
+		return gin.Error{
+			Err:  err,
+			Type: 0,
+			Meta: nil,
+		}
+	}
+	err = os.Mkdir("./USERFILE", 0777)
+	if err!= nil {
+		routes.ErrorResponse(c, 500, "创建文件夹失败")
+		return gin.Error{
+			Err:  err,
+			Type: 0,
+			Meta: nil,
+		}
+	}
+	err = c.SaveUploadedFile(file, "./USERFILE/"+file.Filename)
+	if err!= nil {
+		routes.ErrorResponse(c, 500, "保存文件失败")
+		return gin.Error{
+			Err:  err,
+			Type: 0,
+			Meta: nil,
+		}
+
+	}
+	routes.SuccessResponse(c, 200, "上传文件成功", gin.H{
+		"filename": file.Filename,
+	})
+	return nil
+}
+func uploadFile(c *gin.Context) error {
+	// 上传文件
+	file, err : = c.FormFile("file")
+	if err != nil {
+		routes.ErrorResponse(c,400, "上传文件失败")
+		return gin.Error{
+			Err:  err,
+			Type: 0,
+			Meta: nil,
+		}
+	}
+	err = os.Mkdir("./USERFILE",0777)
+	if err != nil {
+		routes.ErrorResponse(c,500, "创建文件夹失败")
+		return gin.Error{
+			Err:  err,
+			Type: 0,
+			Meta: "Create folder failed",
+		}
+	}
+	err = c.SaveUploadedFile(file, "./USERFILE/"+file.Filename)
+	if err!= nil {
+		routes.ErrorResponse(c,500, "保存文件失败")
+		return gin.Error{
+			Err:  err,
+			Type: 0,
+			Meta: "Save file failed",
+		}
+	}
+	routes.SuccessResponse(c,200, "上传文件成功", gin.H{
+		"filename": file.Filename,
+	})
+	return nil
+}
+func uploadVideo(c *gin.Context) error {
+	file, err := c.FormFile("file")
+	if err!= nil {
+		routes.ErrorResponse(c, 400, "上传文件失败")
+		return gin.Error{
+			Err:  err,
+			Type: 0,
+			Meta: nil,
+		}
+	}
+	err = os.Mkdir("./USERFILE", 0777)
+	if err!= nil {
+		routes.ErrorResponse(c, 500, "创建文件夹失败")
+		return gin.Error{
+			Err:  err,
+			Type: 0,
+			Meta: nil,
+		}		
+	}
+	err = c.SaveUploadedFile(file, "./USERFILE/"+file.Filename)
+	if err!= nil {
+		routes.ErrorResponse(c, 500, "保存文件失败")
+		return gin.Error{
+			Err:  err,
+			Type: 0,
+			Meta: nil,
+		}
+	}
+	routes.SuccessResponse(c, 200, "上传文件成功", gin.H{
+		"filename": file.Filename,
+	})
+}
+func uploadPicture(c *gin.Context) error {
+	file, err := c.FormFile("file")
+	if err!= nil {
+		routes.ErrorResponse(c, 400, "上传文件失败")
+		return gin.Error{
+			Err:  err,
+			Type: 0,
+			Meta: nil,
+		}
+	}
+	err = os.Mkdir("./USERFILE", 0777)
+	if err!= nil {
+		routes.ErrorResponse(c, 500, "创建文件夹失败")
+		return gin.Error{
+			Err:  err,
+			Type: 0,
+			Meta: nil,
+		}
+	}
+	err = c.SaveUploadedFile(file, "./USERFILE/"+file.Filename)
+	if err!= nil {
+		routes.ErrorResponse(c, 500, "保存文件失败")
+		return gin.Error{
+			Err:  err,
+			Type: 0,
+			Meta: nil,
+		}
+	}
+	routes.SuccessResponse(c, 200, "上传文件成功", gin.H{
+		"filename": file.Filename,
+	})
+	return nil
+}
+func downloadDocument(c *gin.Context) error {
+	
 }
 func downloadMusic(c *gin.Context) {
 
