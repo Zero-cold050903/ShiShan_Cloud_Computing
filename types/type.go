07b915575ea1aef6001d78ca1d21d9b73c3b5bbb
dylanliu <pxyforeverlove1314@gmail.com>
2025-05-30 06:17:31 +0800

    New features

--- types/type.go
+++ types/type.go
@@ -23,19 +23,19 @@ type TaskQueue struct {
 	Queue    [config.MAX_TASK_NUM]Task
 }
 type TaskHandler interface {
-	AddTask(task TaskQueue) error           // 添加任务
-	DeleteTask(task TaskQueue) error        // 删除任务
-	UpdateTask(task TaskQueue) error        // 更新任务
-	GetTask(task TaskQueue) error           // 获取任务
-	GetAllTask(task TaskQueue) error        // 获取所有任务
-	RunTask(task TaskQueue) error           // 运行任务
-	StopTask(task TaskQueue) error          // 停止任务
-	RestartTask(task TaskQueue) error       // 重启任务
-	SaveTask(task TaskQueue) error          // 保存任务
-	LoadTask(task TaskQueue) error          // 加载任务
-	SaveAllTask(task TaskQueue) error       // 保存所有任务
-	LoadAllTask(task TaskQueue) error       // 加载所有任务
-	SaveTaskToFile(task TaskQueue) error    // 保存任务到文件
-	LoadTaskFromFile(task TaskQueue) error  // 从文件加载任务
-	SaveAllTaskToFile(task TaskQueue) error // 保存所有任务到文件
+	AddTask() error           // 添加任务
+	DeleteTask() error        // 删除任务
+	UpdateTask() error        // 更新任务
+	GetTask() error           // 获取任务
+	GetAllTask() error        // 获取所有任务
+	RunTask() error           // 运行任务
+	StopTask() error          // 停止任务
+	RestartTask() error       // 重启任务
+	SaveTask() error          // 保存任务
+	LoadTask() error          // 加载任务
+	SaveAllTask() error       // 保存所有任务
+	LoadAllTask() error       // 加载所有任务
+	SaveTaskToFile() error    // 保存任务到文件
+	LoadTaskFromFile() error  // 从文件加载任务
+	SaveAllTaskToFile() error // 保存所有任务到文件
 }
